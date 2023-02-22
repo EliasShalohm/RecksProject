98b9475c5d1fd14606e054ffb0b4e7cefde16b8b
EliasShalohm <eliasshalohm@gmail.com>
2023-02-22 13:00:01 -0600

    Changed Diagrams & Implemented Frames For Types "Class" & "Professor"; "Day" is an Enum included within "Class"

---
+++ RecksWebservice/Types/Class.cs
@@ -0,0 +1,34 @@
+﻿namespace RecksWebservice.Types
+{
+    public enum Day
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    public class Class
+    {
+        private int _totalEnrollCount;
+        private int _availableSlots;
+        private Day[] _days;
+        private int _startTime;
+        private int _endTime;
+        private bool _isFull;
+        private bool _isNightClass;
+        private Class[] _labs;
+        private Professor _professor;
+
+        public Class() {
+                    
+        }
+
+        //Implement Methods
+
+    }
+
+
+}
---
+++ RecksWebservice/Types/Professor.cs
@@ -0,0 +1,26 @@
+﻿namespace RecksWebservice.Types
+{
+    public class Professor
+    {
+        private string _name;
+        private int _rating;
+        private string[] _reviews;
+
+        public Professor(string name, int rating, string[] reviews)
+        {
+            _name = name;
+            _rating = rating;
+            _reviews = reviews;
+        }
+
+        public Professor()
+        {
+            _name = string.Empty;
+            _rating = 0;
+            _reviews = Array.Empty<string>();
+        }
+
+        //Implement Methods
+
+    }
+}
