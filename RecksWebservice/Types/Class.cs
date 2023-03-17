1aea6cc93421619285f7d9db170b00680229ba7c
SeifSultan03 <ssulta7@lsu.edu>
2023-03-16 22:08:13 -0500

    implemented ClassDataHandler. Test Console.Writeline() methods are still there to watch for errors.
    Expanded Class to store states like isNightCLass and extra class Info

--- RecksWebservice/Types/Class.cs
+++ RecksWebservice/Types/Class.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 
 namespace RecksWebservice.Types
 {
@@ -21,6 +22,7 @@ namespace RecksWebservice.Types
         private int _section = 0;
         private int _totalEnrollCount;
         private int _availableSlots;
+        private double _credits;
         private List<Day> _days;
         private int _startTime;
         private int _endTime;
@@ -28,12 +30,14 @@ namespace RecksWebservice.Types
         private bool _isNightClass;
         private List<Class> _labs;
         private Professor _professor;
+        private bool _isTBAClass = false;
+        private string _classInfo;
 
         /// <summary>
         /// Constructors
         /// </summary>
         #region Constructors
-        public Class()  {
+        public Class() {
             _totalEnrollCount = 0;
             _availableSlots = 0;
             _days = new List<Day>();
@@ -51,7 +55,7 @@ namespace RecksWebservice.Types
 
             if (availAbleSlots == 0)
                 _isFull = true;
-            
+
             _days = days;
             _startTime = startTime;
             _endTime = endTime;
@@ -87,19 +91,21 @@ namespace RecksWebservice.Types
             _isFull = isNightClass;
             _isNightClass = isNightClass;
             _labs = new();
-            _professor= new();
+            _professor = new();
         }
-		#endregion
+        #endregion
 
-		/// <summary>
-		/// An assortment of GET-methods to access variables outside of class.
-		/// </summary>
-		/// <returns></returns>
-		#region An assortment of GET-methods to access variables outside of class.
-		public string GetClassID() => _classID;
-		public int GetSection() => _section;
-		public int GetTotalEnrollCount() => _totalEnrollCount;
+        /// <summary>
+        /// An assortment of GET-methods to access variables outside of class.
+        /// </summary>
+        /// <returns></returns>
+        #region An assortment of GET-methods to access variables outside of class.
+        public string GetClassID() => _classID;
+        public int GetSection() => _section;
+        public int GetTotalEnrollCount() => _totalEnrollCount;
         public int GetAvailableSlots() => _availableSlots;
+        public double GetCredits() => _credits;
+        public bool GetTBAStatus() => _isTBAClass;
         public List<Day> GetDays() => _days;
         public int GetStartTime() => _startTime;
         public int GetEndTime() => _endTime;
@@ -107,8 +113,8 @@ namespace RecksWebservice.Types
         public bool CheckNightClassState() => _isNightClass;
         public List<Class> GetLabs() => _labs;
         public Professor GetProfessor() => _professor;
+        public string GetClassInfo() => _classInfo;
         #endregion
-
         /// <summary>
         /// Methods that are utilized for SET operations for instances.
         /// </summary>
@@ -118,41 +124,53 @@ namespace RecksWebservice.Types
         public void SetClassSection(int section) => _section = section;
         public void SetTotalEnrollCount(int count) => _totalEnrollCount = count;
         public void SetAvailableSlots(int count) => _availableSlots = count;
+        public void SetCredits(double credits) => _credits = credits;
+        public void SetTBAStatus(bool isTBAClass) => _isTBAClass = isTBAClass;
         public void AddDay(Day day) => _days.Add(day);
         public void RemoveDay(Day day) => _days.Remove(day);
+        public void SetFullState(bool full) => _isFull = full;
         public void SetDays(List<Day> days) => _days = days;
         public void SetStartTime(int startTime) => _startTime = startTime;
         public void SetEndTime(int endTime) => _endTime = endTime;
         public void SetNightClass(bool isNightClass) => _isNightClass = isNightClass;
         public void AssignLabs(List<Class> labs) => _labs = labs;
-        public void SetProffessor(Professor professor) => _professor = professor;
+        public void AddLab(Class lab) => _labs.Add(lab);   
+        public void SetProfessor(Professor professor) => _professor = professor;
+        public void SetClassInfo(string classInfo) => _classInfo = classInfo;
+        public void AddClassInfo(string classInfo) => _classInfo += "\n" + classInfo;
         #endregion
-
+    
         public void TestForValues() ///Is this needed? -Z
         {
             // Get the current enrollment count
             int currentEnrollCount = GetTotalEnrollCount();
             Console.WriteLine($"Current enrollment count: {currentEnrollCount}");
 
-            // Get the updated enrollment count
-            currentEnrollCount = GetTotalEnrollCount();
-            Console.WriteLine($"Updated enrollment count: {currentEnrollCount}");
+            Console.WriteLine($"availabe slot count: {GetAvailableSlots()}");
 
-            //Cancel enrollment for a student();
-            Console.WriteLine("Canceled enrollment for a student.");
+            Console.WriteLine($"get section: {GetSection()}");
 
-            //Get  the updated enrollment count
-            currentEnrollCount = GetTotalEnrollCount();
-            Console.WriteLine($"Updated enrollment count: {currentEnrollCount}");
+			Console.WriteLine($"classId: {GetClassID()}");
 
-            //Set the enrollment count to a temporary value
-            SetTotalEnrollCount(20);
-            Console.WriteLine("Set enrollment count to 20.");
+			Console.WriteLine($"days: {GetDays()}");
 
-            //Get the updated enrollment count
-            currentEnrollCount = GetTotalEnrollCount();
-            Console.WriteLine("$Updated enrollment count: {currentEnrollCount}");
-        }
+			Console.WriteLine($"startTime: {GetStartTime()}");
+
+			Console.WriteLine($"ENDTime: {GetEndTime()}");
+
+			Console.WriteLine($"isFull: {GetFullState()}");
+
+			Console.WriteLine($"isTBA: {GetTBAStatus()}");
+
+            Console.WriteLine($"isNightClass: {CheckNightClassState()}");
+
+            if (GetLabs().Count > 0)
+            {
+                Class lab = GetLabs().First();
+				Console.WriteLine($"Lab ----------------------- Lab");
+                lab.TestForValues();
+			}
+		}
 
     }
 
