31c3dc5a6df26e10484829e6d940570e24e04009
kjeff18 <124198583+kjeff18@users.noreply.github.com>
2023-03-21 14:32:06 -0500

    styling

--- RecksWebservice/User.cs
+++ RecksWebservice/User.cs
@@ -1,10 +1,51 @@
 ﻿using System;
+using RecksWebservice.Types;
 namespace RecksWebservice
 {
 	public class User
 	{
-		public User()
+        private List<Class> currentClasses = new List<Class>();
+        private int currentHoursScheduled = 0;
+        private int maximumHours = 19;
+
+        public User()
 		{
+ 
+		}
+
+		public void AddClass()
+		{
+
+		}
+
+		public void CheckIfHoursAreLegal()
+		{
+
+		}
+
+		public void DisplayHours()
+		{
+
+		}
+
+		private void getClasses()
+		{
+
+		}
+
+		public void OpenScheduler()
+		{
+
+		}
+
+		public void RemoveClass()
+		{
+
+		}
+
+		private void UpdateUI()
+		{
+
 		}
 	}
 }
