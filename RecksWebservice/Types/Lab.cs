b65317b52bb05e19b1b6c9f3c8c68cd6868a9e1d
dsaulny <dsauln1@lsu.edu>
2023-04-11 14:44:41 -0500

    Added outline for RegularClass.cs and LabClass.cs

--- RecksWebservice/Types/Lab.cs
+++ RecksWebservice/Types/Lab.cs
@@ -1,11 +1,88 @@
 ﻿using System;
 namespace RecksWebservice.Types
 {
-	public class Lab
+	public class Lab : ClassBuilder
 	{
-		public Lab()
-		{
-		}
-	}
+        public override void SetTotalEnrollCount()
+        {
+            classObject._totalEnrollCount = 0;
+        }
+        public override void SetAvailableSlots()
+        {
+            classObject._availableSlots = 0;
+        }
+        public override void SetClassName()
+        {
+            classObject._className = "";
+        }
+        public override void SetClassID()
+        {
+            classObject._classID = "";
+        }
+        public override void SetClassType()
+        {
+            classObject._classType = "";
+        }
+        public override void SetSection()
+        {
+            classObject._section = 0;
+        }
+        public override void SetCredits()
+        {
+            classObject._credits = 0;
+        }
+        public override void SetCourseTitle()
+        {
+            classObject._courseTitle = "";
+        }
+        public override void SetStartTimes()
+        {
+            //classObject._startTimes = 
+        }
+        public override void SetEndTimes()
+        {
+            //classObject._endTimes = 
+        }
+        public override void SetDays()
+        {
+            //classObject._days =
+        }
+        public override bool SetIsFull()
+        {
+            return false;
+        }
+        public override bool SetIsNightClass()
+        {
+            return false;
+        }
+        public override void SetLabs()
+        {
+            //classObject._labs =
+        }
+        public override void SetProfessor()
+        {
+            //classObject._professor =
+        }
+        public override bool SetTBAClass()
+        {
+            return false;
+        }
+        public override void SetSpecialEnrollment()
+        {
+            classObject._specialEnrollment = "";
+        }
+        public override void SetRoomNumber()
+        {
+            classObject._roomNumber = "";
+        }
+        public override void SetCourseBuilding()
+        {
+            classObject._courseBuilding = "";
+        }
+        public override void SetColor()
+        {
+            classObject._colour = "";
+        }
+    }
 }
 
